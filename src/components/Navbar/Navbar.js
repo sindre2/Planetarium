f476eff8327a7ec861a2fdeee6b2f6d9f5ee9990
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-10-10 13:37:39 +0200

    navbar layout change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,39 @@ const Navbar = () => {
   return (
     <>
       <Nav>
+        <NavItems to="/home">Planetarium</NavItems>
+
         <NavList>
-          <NavItems to="/home">Planetarium</NavItems>
-          <NavItems to="/mercury">Mercury</NavItems>
-          <NavItems to="/venus">Venus</NavItems>
-          <NavItems to="/earth">Earth</NavItems>
-          <NavItems to="/mars">Mars</NavItems>
-          <NavItems to="/jupiter">Jupiter</NavItems>
-          <NavItems to="/saturn">Saturn</NavItems>
-          <NavItems to="/uranus">Uranus</NavItems>
-          <NavItems to="/neptune">Neptune</NavItems>
-          <NavItems to="/pluto">Pluto</NavItems>
-          <NavItems to="/contact">Contact</NavItems>
+          <li>
+            <NavItems to="/mercury">Mercury</NavItems>
+          </li>
+          <li>
+            <NavItems to="/venus">Venus</NavItems>
+          </li>
+          <li>
+            <NavItems to="/earth">Earth</NavItems>
+          </li>
+          <li>
+            <NavItems to="/mars">Mars</NavItems>
+          </li>
+          <li>
+            <NavItems to="/jupiter">Jupiter</NavItems>
+          </li>
+          <li>
+            <NavItems to="/saturn">Saturn</NavItems>
+          </li>
+          <li>
+            <NavItems to="/uranus">Uranus</NavItems>
+          </li>
+          <li>
+            <NavItems to="/neptune">Neptune</NavItems>
+          </li>
+          <li>
+            <NavItems to="/pluto">Pluto</NavItems>
+          </li>
         </NavList>
+
+        <NavItems to="/contact">Contact</NavItems>
       </Nav>
     </>
   );
