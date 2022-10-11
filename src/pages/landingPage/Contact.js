2bf62f0a582dbc7636b18015e27ffeb4c9068ae6
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-10-11 14:26:20 +0200

    fixes for merge contactForm

--- src/pages/landingPage/Contact.js
+++ src/pages/landingPage/Contact.js
@@ -1,20 +1,16 @@
-import React,{useState} from "react";
-import { Form } from "../../components/formcomponents/Form";
-import "../../components/formcomponents/formstyle.css"
+import React, { useState } from "react";
 
-import { FormTwo } from "../../components/formtwo/Formtwo";
-const Contact = () => {
+import { Form } from "../../components/formComponents/Form";
+
+const Contact2 = () => {
   const pageName = "Contact";
-
-  
 
   return (
     <div>
       <h1>{pageName}</h1>
       <Form />
-     
     </div>
   );
 };
 
-export default Contact;
+export default Contact2;
