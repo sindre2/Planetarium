2ce0b5dc6ba9f2e39028019aaaf02992cd432bf5
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-10-12 10:30:00 +0200

    added animation to navBar

--- src/components/Navbar/navbarStyles.js
+++ src/components/Navbar/navbarStyles.js
@@ -6,7 +6,6 @@ export const Nav = styled.nav`
   font-size: 1.5em;
   text-align: center;
   height: 100px;
-  background: #262321;
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -16,20 +15,46 @@ export const NavList = styled.ul`
   margin: 0;
   padding: 0;
   display: flex;
-  flex: row;
+  height: 100%;
+  & li {
+    height: 100%;
+    margin-right: 0.625rem;
+  }
+  & li:last-child {
+    margin-right: 0;
+  }
+  & li:after {
+    content: "";
+    display: block;
+    border-bottom: solid 3px ${(props) => props.theme.colors.nav_link_hover};
+    transform: scaleX(0);
+    transition: transform 250ms ease-in-out;
+  }
+  & li:hover:after {
+    transform: scaleX(1);
+  }
+`;
+export const NavListMain = styled(NavList)`
+  min-width: 40%;
+  justify-content: space-around;
 `;
 export const NavItems = styled(NavLink)`
+  font-family: ${(props) => props.theme.fonts.header}, sans-serif;
   background: brown;
   color: white;
-  height: 2rem;
-  margin-right: 0.625rem;
+  height: 99%;
+  display: flex;
+  align-items: center;
   background: none;
   border: none;
+  border-bottom: 3px solid transparent;
   font-size: 1.5rem;
   font-weight: 700;
   text-decoration: none;
+  text-transform: uppercase;
+  transition: color 250ms ease-in;
   cursor: pointer;
   &:hover {
-    color: #fb6012;
+    color: ${(props) => props.theme.colors.nav_link_hover};
   }
 `;
